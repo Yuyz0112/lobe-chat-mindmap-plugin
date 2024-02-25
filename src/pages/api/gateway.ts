a1c75f5e064c791fe81d3c7d539f0cccfa0ec088
Yanzhen Yu <yanzhen@smartx.com>
2024-02-25 23:02:17 +0800

    remove gateway

--- src/pages/api/gateway.ts
+++ src/pages/api/gateway.ts
@@ -1,7 +1,13 @@
-import { createLobeChatPluginGateway } from '@lobehub/chat-plugins-gateway';
-
 export const config = {
   runtime: 'edge',
 };
 
-export default createLobeChatPluginGateway();
+export default async (req: Request) => {
+  if (process.env.NODE_ENV === 'development') {
+    const { createGatewayOnEdgeRuntime } = await import('@lobehub/chat-plugins-gateway');
+
+    return createGatewayOnEdgeRuntime()(req);
+  }
+
+  return new Response('gateway');
+};
